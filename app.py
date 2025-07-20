fe0d53101b787a259516328445227a87d51122fd
GRBI88 <gbiernacki@o2.pl>
2025-07-20 19:44:22 +0200

    gitignore

--- app.py
+++ app.py
@@ -1,10 +1,11 @@
-import requests
+import requests, json
 
 for user in range(1, 6):
     getRequests = requests.get('https://randomuser.me/api/')
     if getRequests.status_code == 200:
         userData = getRequests.json()
         print(f"User {user}: {userData['results'][0]['name']['first']} {userData['results'][0]['name']['last']}")
+        json.dump(userData, open(f"out/user_{user}.json", "w"), indent=4)    
     else:
         print(f"Failed to retrieve user {user}. Status code: {getRequests.status_code}")
         print(f"Error: {getRequests.text}")
