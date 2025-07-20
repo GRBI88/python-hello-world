c430b5e52b8e7b2e55af47c96debe9270cd7f54d
GRBI88 <gbiernacki@o2.pl>
2025-07-20 14:51:45 +0200

    commit to master

---
+++ app.py
@@ -0,0 +1,15 @@
+import requests
+
+for user in range(1, 6):
+    getRequests = requests.get('https://randomuser.me/api/')
+    if getRequests.status_code == 200:
+        userData = getRequests.json()
+        print(f"User {user}: {userData['results'][0]['name']['first']} {userData['results'][0]['name']['last']}")
+    else:
+        print(f"Failed to retrieve user {user}. Status code: {getRequests.status_code}")
+        print(f"Error: {getRequests.text}")
+        break
+else:
+    print("All users retrieved successfully.") 
+
+
